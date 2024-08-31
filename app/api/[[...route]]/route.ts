13ca1f34b02bf59e877183b8de0576f0ebae67b8
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-31 10:27:06 +0300

    Hono.js setup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,16 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+// routes
+import authors from "./authors";
+import books from "./books";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!"
-  });
-});
+app.route("/authors", authors);
+app.route("/books", books);
 
 export const GET = handle(app);
 export const POST = handle(app);
