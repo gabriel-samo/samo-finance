052ed980c9863de32534382527362e25007717a3
Gabriel Samoylov <gsamo122@gmail.com>
2024-09-03 19:29:57 +0300

    cloudflare test

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -5,49 +5,41 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkOptions } from "@/utils/clerkOpions";
 
-export const runtime = "edge";
+const app = new Hono().get("/", clerkMiddleware(clerkOptions), async (c) => {
+  // try {
+  const auth = getAuth(c);
 
-const app = new Hono().get(
-  "/",
-  clerkMiddleware({
-    secretKey: process.env.CLERK_SECRET_KEY,
-    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-  }),
-  async (c) => {
-    try {
-      const auth = getAuth(c);
-
-      if (!auth?.userId) {
-        // NEW version of Hono
-        return c.json({ error: "Unauthorized" }, 401);
-        // OLD version of Hono
-        // throw new HTTPException(401, {
-        //   res: c.json({ error: "Unauthorized" }, 401)
-        // });
-      }
-
-      const data = await db
-        .select({
-          id: accounts.id,
-          name: accounts.name
-        })
-        .from(accounts)
-        .where(eq(accounts.userId, auth.userId));
-      return c.json({ data });
-    } catch (error: any) {
-      console.error("Error fetching accounts:", error);
-      return c.json(
-        {
-          message: error.message,
-          stack: error.stack,
-          cause: error.cause,
-          name: error.name
-        },
-        500
-      );
-    }
+  if (!auth?.userId) {
+    // NEW version of Hono
+    return c.json({ error: "Unauthorized" }, 401);
+    // OLD version of Hono
+    // throw new HTTPException(401, {
+    //   res: c.json({ error: "Unauthorized" }, 401)
+    // });
   }
-);
+
+  const data = await db
+    .select({
+      id: accounts.id,
+      name: accounts.name
+    })
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
+  return c.json({ data });
+  // } catch (error: any) {
+  //   console.error("Error fetching accounts:", error);
+  //   return c.json(
+  //     {
+  //       message: error.message,
+  //       stack: error.stack,
+  //       cause: error.cause,
+  //       name: error.name
+  //     },
+  //     500
+  //   );
+  // }
+});
 
 export default app;
---
+++ utils/clerkOpions.ts
@@ -0,0 +1,4 @@
+export const clerkOptions = {
+  secretKey: process.env.CLERK_SECRET_KEY,
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+};
