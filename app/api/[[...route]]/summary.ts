5b74bb3e224d7715c4e9675baac8ea0ce2f33ad1
Gabriel Samoylov <gsamo122@gmail.com>
2024-09-17 17:48:40 +0300

    add: Dashboard charts

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -166,7 +166,7 @@ const app = new Hono().get(
             Number
           ), // Calculate total daily income
         expenses:
-          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ABS(${transactions.amount}) ELSE 0 END)`.mapWith(
             Number
           ) // Calculate total daily expenses
       })
